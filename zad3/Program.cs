d75d0780a2738cba9d259854ad9e738a243da5e6
MEGA <MEGA@gmail.com>
2025-02-14 15:36:53 +0200

    .

---
+++ zad3/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace zad3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write a number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Reversed: {Funk(number, 0)}");
+        }
+        static int Funk(int number, int reversed)
+        {
+            if (number == 0)
+                return reversed;
+            else 
+                return Funk(number / 10, reversed * 10 + number % 10);
+        }
+    }
+}
