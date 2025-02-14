2293302a7cad9561687ea7c8ee58daad91e6121d
Nikito69 <nikiniki2777@gmail.com>
2025-02-14 15:19:35 +0200

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write a number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{Funk(number)}");
+        }
+        static bool Funk(int number)
+        {
+            if (number == 1)
+                return true;
+            else if (number % 2 == 0)
+                return Funk(number / 2);
+            else
+                return false;
+        }
+
+    }
+}
